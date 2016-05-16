86fadc66327cb856535c9ec42d200ed8bc1ab298
hoangthanhlong <hoangthanhlong@gmail.com>
2016-05-16 15:28:42 +0700

    default nargs is list; format score, remove decimal

--- hackerrank_score.py
+++ hackerrank_score.py
@@ -11,7 +11,7 @@ import requests
 def get_hackerrank_score():
     parser = argparse.ArgumentParser()
     parser.add_argument('users', help='Usernames to get score',
-                        default='hoangthanhlong', nargs='*')
+                        default=['hoangthanhlong'], nargs='*')
 
     args = parser.parse_args()
 
@@ -23,7 +23,7 @@ def get_hackerrank_score():
         if res.status_code == 200:
             for field in res.json():
                 if field['name'] == 'Python':
-                    ret.append('{}: {}'.format(user,
+                    ret.append('{}: {:.0f}'.format(user,
                                                field['practice']['score']))
         else:
             ret.append('{}: Not found'.format(user))
