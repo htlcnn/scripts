4cc71df76c9090a3ab671ca95fe15ddc99d2a612
hoangthanhlong <hoangthanhlong@gmail.com>
2016-05-16 08:54:50 +0700

    add hackerrank_score.py

---
+++ hackerrank_score.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''
+Get Python Archive score on hackerrank
+'''
+
+import argparse
+import requests
+
+
+def get_hackerrank_score():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('users', help='Usernames to get score',
+                        default='hoangthanhlong', nargs='*')
+
+    args = parser.parse_args()
+
+    ret = []
+
+    for user in args.users:
+        url = 'https://www.hackerrank.com/rest/hackers/{}/scores2'.format(user)
+        res = requests.get(url)
+        if res.status_code == 200:
+            for field in res.json():
+                if field['name'] == 'Python':
+                    ret.append('{}: {}'.format(user,
+                                               field['practice']['score']))
+        else:
+            ret.append('{}: Not found'.format(user))
+    return ret
+
+if __name__ == '__main__':
+    for info in get_hackerrank_score():
+        print(info)
