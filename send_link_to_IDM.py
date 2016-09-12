f4d3bbd633d63bf83b4f9d092dc89d8a60cdd90d
Long <hoangthanhlong@gmail.com>
2016-09-12 14:40:03 +0700

    use io.open with utf8 encoding, edit docstring

--- send_link_to_IDM.py
+++ send_link_to_IDM.py
@@ -1,6 +1,6 @@
 '''
 Send links from file.txt to IDM
-file.txt content: filename - http://link (' - ' as delimiter)
+file.txt content: http://link * filename(' * ' as delimiter)
 '''
 # http://www.internetdownloadmanager.com/support/idm_api.html
 # interface ICIDMLinkTransmitter2 : ICIDMLinkTransmitter
@@ -31,14 +31,14 @@ file.txt content: filename - http://link (' - ' as delimiter)
 #     1 - do not show any confirmations dialogs; 
 #     2 - add to queue only, do not start downloading.
 # reserved1 - can be used to set a specific user-agent header with the following way:
-#     reserved1.vt = VT_BSTR; reserved1.bstrVal = pbstrUA; if you don’t need to specify a 
+#     reserved1.vt = VT_BSTR; reserved1.bstrVal = pbstrUA; if you don't need to specify a 
 #     user agent, then reserved1.vt should be set to VT_EMPTY;
 # reserved2 - not used, you should set reserved2.vt to VT_EMPTY;
  
 # Paramerets of SendLinksArray function:
-# Location - the referrer of download, it’s assumed that the referrer is a single one 
+# Location - the referrer of download, it's assumed that the referrer is a single one 
 #     for the entire array of internet links.
-# pLinksArray – a pointer to 2 dimensional SAFEARRAY array of BSTR strings. 
+# pLinksArray - a pointer to 2 dimensional SAFEARRAY array of BSTR strings. 
 #     For example, for N number of links, the size of the array will be (4 * N). 
 #     For i changing from 0 to N-1
 #     a[i,0] elements of the array are URLs to download, 
@@ -49,6 +49,7 @@ file.txt content: filename - http://link (' - ' as delimiter)
 import argparse
 import comtypes.client as cc
 import comtypes
+import io
 import os
 
 
@@ -83,9 +84,9 @@ def main():
     parser.add_argument('filename', help='txt file that contains links to download')
     args = parser.parse_args()
     
-    with open(args.filename) as f:
+    with io.open(args.filename, encoding='utf8') as f:
         for line in f:
-            filename, link = line.split(' - ') # custom delimiter here
+            link, filename = line.strip().split(' * ') # custom delimiter here
             send_link_to_idm(link, filename)
     
 if __name__ == "__main__":
